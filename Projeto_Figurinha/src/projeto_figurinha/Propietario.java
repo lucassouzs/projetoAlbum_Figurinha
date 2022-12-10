df8e03f55078e12f48b51ceaa1ef045b7a936529
Lucas Víctor Ferreira de Araújo <98000542+Lucas211063194@users.noreply.github.com>
2022-12-10 18:15:42 -0300

    New commit

--- Projeto_Figurinha/src/projeto_figurinha/Propietario.java
+++ Projeto_Figurinha/src/projeto_figurinha/Propietario.java
@@ -2,9 +2,9 @@ package projeto_figurinha;
 import java.util.ArrayList;
 
 public class Propietario {
-	private String nome;
-	private int idade;
-	private String sexo;
+	private String nomePropietario;
+	private int idadePropietario;
+	private String sexoPropietario;
 	private ArrayList<Album_Figurinha> albumFigurinhas;
 	
 	public Propietario(String nome, int idade, String sexo){
@@ -16,7 +16,7 @@ public class Propietario {
 	// gets e sets 
 	
 	public String getNomePropietario() {
-		return nomePropietario();
+		return nomePropietario;
 	}
 	public void setNomePropietario() {
 		this.nomePropietario = nomePropietario;
@@ -28,7 +28,7 @@ public class Propietario {
 		this.idadePropietario = idadePropietario;
 	}
 	public String getSexoropietario() {
-		return sexoPropietario();
+		return sexoPropietario;
 	}	
 	public void setSexoPropietario() {
 		this.sexoPropietario = sexoPropietario;
@@ -37,7 +37,7 @@ public class Propietario {
 		return  albumFigurinhas;
 	}
 
-	public void setAlbum_Figurinhas(ArrayList<Album_Figurinhas>  albumFigurinhas) {
+	public void setAlbum_Figurinhas(ArrayList<Album_Figurinha>  albumFigurinhas) {
 		this. albumFigurinhas =  albumFigurinhas;
 	}
 	
@@ -45,4 +45,4 @@ public class Propietario {
 
 
 	}
-}
+
