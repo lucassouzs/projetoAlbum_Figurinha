d5e5a9e9fc174f40abde8f98c01d1b42fd3c73fa
Lucas13032003 <lucasvictor.lvfa18@gmail.com>
2023-01-28 10:44:23 -0300

    Inicio dos comentario 28/01

--- Projeto_Figurinha/src/view/Main.java
+++ Projeto_Figurinha/src/view/Main.java
@@ -1,4 +1,17 @@
+/**
+ * Classe responsavel por inicializar o programa chamando  MyCup.
+ * @author Lucas Ribeiro de Souza 
+ * @author Lucas Víctor Ferreira de Araújo
+ * @since 2022
+ * @version 1.0
+ */
 package view;
+
+/**
+ * Metodo Main que instancia a classe Panel Entrada e a torna visivel;
+ * 
+ * @param args
+ */
 
 public class Main {
 
